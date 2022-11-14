6684f1fe4e26e9d63e215e60a57b3412be3ddf66
Tolga Bastürk <tolga.bastuerk@outlook.com>
2022-11-14 21:46:57 +0100

    added Timeline and TimelineItem

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import data from "../data/TimelineData";
+import TimelineItem from "./TimelineItem";
 
 const Timeline = () => {
-  return <div>Timeline</div>;
+  return (
+    <div className="flex flex-col md:flex-row justify-center my-20">
+      <div className="w-full md:w-7/12">
+        {data.map((item) => (
+          <TimelineItem
+            year={item.year}
+            title={item.title}
+            duration={item.duration}
+            details={item.details}
+          />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Timeline;
---
+++ src/data/TimelineData.js
@@ -0,0 +1,30 @@
+export default [
+  {
+    year: "Today",
+    title: "Lorem Ipsum Academy",
+    duration: "4 years",
+    details:
+      "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.",
+  },
+  {
+    year: "2019",
+    title: "Lorem Ipsum Academy",
+    duration: "3 years",
+    details:
+      "At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
+  },
+  {
+    year: "2017",
+    title: "Lorem Ipsum Academy",
+    duration: "2 years",
+    details:
+      "Consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.",
+  },
+  {
+    year: "2016",
+    title: "Lorem Ipsum Academy",
+    duration: "1 years",
+    details:
+      "Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. ",
+  },
+];
